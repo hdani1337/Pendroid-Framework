f84b0fc0339e647a570d983309e0b085b316372a
Horváth Dániel <admin@hdani1337.hu>
2020-06-23 12:29:01 +0200

    Initial commit

---
+++ android/src/hu/hdani1337/framework/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.hdani1337.framework;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.hdani1337.framework.Framework;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Framework(), config);
+	}
+}
